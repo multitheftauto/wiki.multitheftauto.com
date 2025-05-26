8a4e4e0ecd185c9c6f96904a3d606460c9e3dde9
FileEX <kongali@interia.pl>
2025-05-26 07:59:14 +0200

    Fix syntax highlighting for other language than lua
    Other languages didn’t have syntax highlighting because our custom style is tailored specifically for Lua and might not include all necessary tags. That’s why we load the syntax from another theme (GitHub Dark) for other languages and override only the Lua language.

--- web/ec.config.mjs
+++ web/ec.config.mjs
@@ -1,16 +1,25 @@
-import fs from 'fs';
+import { readFileSync } from 'fs';
+
+import githubDark from 'tm-themes/themes/github-dark.json' with { type: 'json' };
+const mtaLuaDark = JSON.parse(readFileSync('./src/themes/mtasa_lua-theme_dark.json', 'utf-8'));
+
+const hybridTheme = {
+  ...githubDark,
+  tokenColors: [
+    ...githubDark.tokenColors,
+    ...mtaLuaDark.tokenColors,
+  ],
+  colors: [mtaLuaDark.colors]
+};
 
 export default {
-  themes: [
-    JSON.parse(fs.readFileSync('./src/themes/mtasa_lua-theme_dark.json', 'utf-8')),
-    JSON.parse(fs.readFileSync('./src/themes/mtasa_lua-theme_light.json', 'utf-8')),
-  ],
+  themes: [hybridTheme],
   shiki: {
     langs: [
       {
         id: 'lua',
         scopeName: 'source.lua.mta',
-        ...JSON.parse(fs.readFileSync('./src/grammars/lua-mta.tmLanguage.json', 'utf-8')),
+        ...JSON.parse(readFileSync('./src/grammars/lua-mta.tmLanguage.json', 'utf-8')),
       },
     ],
   },
