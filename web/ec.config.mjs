297ab45fc1f7f3dbaec3e9a61fe994463b7ebaea
FileEX <kongali@interia.pl>
2025-05-26 09:41:41 +0200

    Fix light theme syntax highlighting

--- web/ec.config.mjs
+++ web/ec.config.mjs
@@ -1,19 +1,37 @@
 import { readFileSync } from 'fs';
 
 import githubDark from 'tm-themes/themes/github-dark.json' with { type: 'json' };
-const mtaLuaDark = JSON.parse(readFileSync('./src/themes/mtasa_lua-theme_dark.json', 'utf-8'));
+import githubLight from 'tm-themes/themes/github-light.json' with { type: 'json' };
 
-const hybridTheme = {
+const mtaLuaDark = JSON.parse(readFileSync('./src/themes/mtasa_lua-theme_dark.json', 'utf-8'));
+const mtaLuaLight = JSON.parse(readFileSync('./src/themes/mtasa_lua-theme_light.json', 'utf-8'));
+
+const hybridDarkTheme = {
   ...githubDark,
   tokenColors: [
     ...githubDark.tokenColors,
     ...mtaLuaDark.tokenColors,
   ],
-  colors: [mtaLuaDark.colors]
+  colors: {
+    ...githubDark.colors,
+    ...mtaLuaDark.colors
+  }
+};
+
+const hybridLightTheme = {
+  ...githubLight,
+  tokenColors: [
+    ...githubLight.tokenColors,
+    ...mtaLuaLight.tokenColors,
+  ],
+  colors: {
+    ...githubLight.colors,
+    ...mtaLuaLight.colors
+  }
 };
 
 export default {
-  themes: [hybridTheme],
+  themes: [hybridDarkTheme, hybridLightTheme],
   shiki: {
     langs: [
       {
@@ -21,6 +39,6 @@ export default {
         scopeName: 'source.lua.mta',
         ...JSON.parse(readFileSync('./src/grammars/lua-mta.tmLanguage.json', 'utf-8')),
       },
-    ],
-  },
+    ]
+  }
 };
