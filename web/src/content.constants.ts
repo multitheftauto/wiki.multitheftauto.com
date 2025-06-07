291db084b0ab2effbe3b0055d20f587f442aa209
Fernando-A-Rocha <frocha2001@gmail.com>
2025-06-07 13:55:44 +0100

    Make contributing URL a constant

--- web/src/content.constants.ts
+++ web/src/content.constants.ts
@@ -1,5 +1,6 @@
 export const SITE_TITLE = 'Multi Theft Auto: Wiki';
 export const SITE_URL = 'https://wiki.preview.multitheftauto.com';
+export const SITE_CONTRIBUTING_URL = "https://github.com/multitheftauto/wiki.multitheftauto.com/blob/main/CONTRIBUTING.md";
 export const OLD_WIKI_URL = 'https://wiki.multitheftauto.com';
 export const OLD_WIKI_REDIRECT = true; // Set to false to disable redirecting to old wiki on 404
 export const MTA_CURRENT_VERSION = {
