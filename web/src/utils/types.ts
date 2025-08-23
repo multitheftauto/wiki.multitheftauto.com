397f618311a38659459c475fc7b8db0ac33ef0de
FileEX <kongali@interia.pl>
2025-08-23 20:10:44 +0200

    Add issues table to function page

--- web/src/utils/types.ts
+++ web/src/utils/types.ts
@@ -1,6 +1,95 @@
+import type { getCollection } from "astro:content";
+
 export type FunctionType = 'shared' | 'client' | 'server';
 export type NotesTypeType = 'info' | 'warning' | 'important' | 'tip' | 'needs_checking';
 export type NotesType = {
     type: NotesTypeType
     content: string;
 }[];
+
+export type FunctionItem = Awaited<ReturnType<typeof getCollection>>[number];
+
+export type BaseOOP = {
+  element: string;
+  note?: string;
+};
+
+// /!\ constructor is a reserved word in JS/TS, so we use constructorclass
+export type MethodOOP = BaseOOP & {
+  method: string;
+  static: boolean;
+  variable?: string;
+  constructorclass?: never; // mutually exclusive with constructor
+};
+
+export type ConstructorOOP = BaseOOP & {
+  constructorclass: string;
+  method?: never;      // mutually exclusive with method
+  static?: never;
+  variable?: never;
+};
+
+export type OOPInfo = MethodOOP | ConstructorOOP;
+
+export type FunctionInfo = {
+    description: string;
+    incomplete?: boolean;
+    type: FunctionType;
+    typePretty: string;
+    pair?: string;
+    preview_images?: string[];
+    oop?: OOPInfo;
+    notes?: NotesType;
+    version?: VersionInfo;
+    issues?: IssuesTable;
+};
+
+export type FunctionData = {
+    shared?: any;
+    client?: any;
+    server?: any;
+};
+
+export type VersionInfo = {
+  added?: string;
+  updated?: string;
+  removed?: string;
+};
+
+export type IssuesTable = {
+    id: number|string
+    description: string
+}[];
+
+export type FunctionsByCategory = {
+    [folder: string]: FunctionItem[];
+};
+export type FunctionsByTypeByCategory = {
+    [key in FunctionType]: FunctionsByCategory;
+};
+
+export type Parameter = {
+  name: string;
+  type: string;
+  description: string;
+  default?: string;
+  templateList?: string;
+};
+
+export type ReturnValue = {
+  type: string;
+  name: string;
+  templateList?: string;
+};
+
+export type ReturnBlock = {
+  description?: string;
+  values: ReturnValue[];
+};
+
+export type Syntax = {
+  type: 'shared' | 'server' | 'client';
+  parameters: Parameter[];
+  returns: ReturnBlock | null;
+  syntaxString: string;
+};
