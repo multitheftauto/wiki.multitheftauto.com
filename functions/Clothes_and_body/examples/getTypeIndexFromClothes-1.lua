4a2f71e040ebff1af8a998ecbebeb505e4c97302
FileEX <kongali@interia.pl>
2025-11-29 22:31:14 +0100

    Clothes & body functions

--- functions/Clothes_and_body/examples/getTypeIndexFromClothes-1.lua
+++ functions/Clothes_and_body/examples/getTypeIndexFromClothes-1.lua
@@ -1,17 +1,18 @@
-function scriptPreviousClothes ( thePlayer, key, clothesType )
-  local currentTexture, currentModel = getPedClothes ( thePlayer, clothesType ) -- get the current clothes on this slot
-  local clothesIndex = 1
-  if ( currentTexture ) then -- if he had clothes of that type
-    local tempA, tempB = getTypeIndexFromClothes ( currentTexture, currentModel ) -- get the type and index for these clothes, so we can decrease and get the previous in the list
-    if ( tempA and tempB ) then -- if we found them
-      clothesType, clothesIndex = tempA, tempB
+function scriptPreviousClothes(thePlayer, key, clothesType)
+    local currentTexture, currentModel = getPedClothes(thePlayer, clothesType) -- get the current clothes on this slot
+    local clothesIndex = 1
+    if (currentTexture) then -- if he had clothes of that type
+        local tempA, tempB = getTypeIndexFromClothes(currentTexture, currentModel) -- get the type and index for these clothes, so we can decrease and get the previous in the list
+        if (tempA and tempB) then -- if we found them
+            clothesType, clothesIndex = tempA, tempB
+        end
     end
-  end
-  clothesIndex = clothesIndex - 1
-  local texture, model = getClothesByTypeIndex ( clothesType, clothesIndex ) -- get the new texture and model
-  if ( texture == false ) then -- if we've reached the end of the list
-    removePedClothes ( thePlayer, clothesType )
-  else addPedClothes ( thePlayer, texture, model, clothesType )
-  end
+    clothesIndex = clothesIndex - 1
+    local texture, model = getClothesByTypeIndex(clothesType, clothesIndex) -- get the new texture and model
+    if (not texture) then -- if we've reached the end of the list
+        removePedClothes(thePlayer, clothesType)
+    else
+        addPedClothes(thePlayer, texture, model, clothesType)
+    end
 end
-addCommandHandler ( "previousClothes", scriptPreviousClothes )
+addCommandHandler("previousClothes", scriptPreviousClothes)
