26292c963cfa02565973d5a259fbca891093f3db
FileEX <kongali@interia.pl>
2025-11-29 16:04:29 +0100

    Audio examples improvements (indentions)

--- functions/Audio/examples/setSoundPan_OOP-1.lua
+++ functions/Audio/examples/setSoundPan_OOP-1.lua
@@ -1,7 +1,7 @@
 function playMusic ()
-        local left = playSFX("genrl", 75, 6, true)    -- Play loading theme music
-        local right = playSFX("genrl", 75, 7, true)
-        left.pan = -1                        -- switch the first music to left channel
-        right.pan = 1                         -- switch the second music to right channel
+    local left = playSFX("genrl", 75, 6, true) -- Play loading theme music
+    local right = playSFX("genrl", 75, 7, true)
+    left.pan = -1 -- switch the first music to left channel
+    right.pan = 1 -- switch the second music to right channel
 end
-addCommandHandler("music", playMusic)                 -- add the command handler
+addCommandHandler("music", playMusic) -- add the command handler
--- functions/Audio/examples/setSoundSpeed_OOP-1.lua
+++ functions/Audio/examples/setSoundSpeed_OOP-1.lua
@@ -1,5 +1,5 @@
 function soundFunc()
-local sound = Sound ( "/sounds/jizzy.mp3",true) -- Let's play a sound
-sound.speed = 1.2 -- And it will be a little bit faster !
+    local sound = Sound ( "/sounds/jizzy.mp3",true) -- Let's play a sound
+    sound.speed = 1.2 -- And it will be a little bit faster !
 end
 addCommandHandler("play",soundFunc)
