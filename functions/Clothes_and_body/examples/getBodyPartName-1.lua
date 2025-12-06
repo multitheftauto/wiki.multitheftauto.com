04d61b1d2d80a0ca6b796c312bdc75dc6f6d5db2
FileEX <kongali@interia.pl>
2025-12-06 16:41:55 +0100

    Minor fixes (clothes)
    - Code formatting
    - Typos

--- functions/Clothes_and_body/examples/getBodyPartName-1.lua
+++ functions/Clothes_and_body/examples/getBodyPartName-1.lua
@@ -1,18 +1,18 @@
-function deathMessageOnWasted ( ammo, attacker, weapon, bodypart )
-  if ( attacker ) then                                    -- if we have an attacker
-    if ( getElementType ( attacker ) == "player" ) then   -- make sure the element that killed him was a player
-      tempString = getPlayerName ( attacker ) .. " killed " .. getPlayerName ( source ) .. " (" .. getWeaponNameFromID ( weapon ) .. ")"
-      if ( bodypart == 9 ) then -- if he was shot in the head
-        tempString = tempString .. " (HEADSHOT!)"
-      else
-        tempString = tempString .. " (" .. getBodyPartName ( bodypart ) .. ")"
-      end
-      outputChatBox ( tempString )
+function deathMessageOnWasted(ammo, attacker, weapon, bodypart)
+    if (attacker) then -- if we have an attacker
+        if (getElementType(attacker) == "player") then -- make sure the element that killed him was a player
+            tempString = getPlayerName(attacker) .. " killed " ..getPlayerName(source) .. " (" ..getWeaponNameFromID(weapon) .. ")"
+            if (bodypart == 9) then -- if he was shot in the head
+                tempString = tempString .. " (HEADSHOT!)"
+            else
+                tempString = tempString .. " (" .. getBodyPartName(bodypart) ..")"
+            end
+            outputChatBox(tempString)
+        else
+            outputChatBox(getPlayerName(source) .. " died. (" ..getWeaponNameFromID(weapon) .. ") (" ..getBodyPartName(bodypart) .. ")")
+        end
     else
-      outputChatBox ( getPlayerName ( source ) .. " died. (" .. getWeaponNameFromID ( weapon ) .. ") (" .. getBodyPartName ( bodypart ) .. ")" )
+        outputChatBox(getPlayerName(source) .. " died. (" ..getWeaponNameFromID(weapon) .. ") (" ..getBodyPartName(bodypart) .. ")")
     end
-  else
-    outputChatBox ( getPlayerName ( source ) .. " died. (" .. getWeaponNameFromID ( weapon ) .. ") (" .. getBodyPartName ( bodypart ) .. ")" )
-  end
 end
-addEventHandler ( "onPlayerWasted", root, deathMessageOnWasted )
+addEventHandler("onPlayerWasted", root, deathMessageOnWasted)
