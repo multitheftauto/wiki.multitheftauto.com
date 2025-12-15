951a34d3e06f35828c4312e61532bf05bd4b8096
FileEX <kongali@interia.pl>
2025-12-15 17:08:42 +0100

    Drawing functions improvements and fixes

--- functions/Drawing/examples/dxDrawText-3.lua
+++ functions/Drawing/examples/dxDrawText-3.lua
@@ -34,8 +34,8 @@ function wordWrap(text, maxwidth, scale, font, colorcoded)
     return lines
 end
 
-text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-formatedtext = wordWrap(text, 300, 1, "default", false) --wrap text
+local text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+local formatedtext = wordWrap(text, 300, 1, "default", false) --wrap text
 
 addEventHandler("onClientRender", root, function()
     dxDrawRectangle(5, 5, 300, 200, tocolor(0, 0, 0, 100)) --draw background
