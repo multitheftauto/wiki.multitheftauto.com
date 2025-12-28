5b19f14074001c8316073a2ed2cb084cd9e6a5d2
FileEX <kongali@interia.pl>
2025-12-28 15:18:25 +0100

    Minor fixes (element functions)

--- functions/Element/examples/detachElements_OOP-1.lua
+++ functions/Element/examples/detachElements_OOP-1.lua
@@ -1,6 +1,6 @@
 function freeElement(theElement)
     if (theElement.attached) then -- If the specified element is attached to something
-        theElement.attached:detach() -- Detach it.
+        theElement:detach() -- Detach it.
     else
         outputChatBox("Element is not attached") -- If not, say it wasn't attached in the first place.
     end
