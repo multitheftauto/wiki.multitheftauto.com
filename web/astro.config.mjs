dc47d0341a12615fef531ae87738094ffdb0df64
FileEX <kongali@interia.pl>
2026-08-31 23:55:27 +0200

    Linux manual

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -86,8 +86,9 @@ export default defineConfig({
         {
           label: "Guides",
           items: [
-            {label: "Scripting introduction", link: "/guides/Scripting-introduction"},
+            {label: "Client on Linux manual", link: "/guides/Client-on-Linux-manual"},
             {label: "Steam Deck manual", link: "/guides/SteamDeck"},
+            {label: "Scripting introduction", link: "/guides/Scripting-introduction"},
             {label: "Introduction to scripting GUI", link: "/guides/GUI-introduction"},
             {label: "Script security", link: "/guides/Script-security"},
             {label: "Introduction to CEF", link: "/guides/CEF-introduction"},
